22baa9b7dda0de0e80cf75bf068d0de270021158
Laxman Munigala <laxman.225@gmail.com>
2025-12-21 15:51:28 -0500

    Added perplexity models

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import asyncio
 from google.adk.runners import InMemoryRunner
 from google.genai import types
 from stock_analysis_agent.agent import root_agent
-import logging
+import logging,random
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 )
 
@@ -13,20 +13,29 @@ async def main():
     # Use InMemoryRunner which uses in-memory session and artifact services
     # Using async context manager for automatic cleanup (calls runner.close())
     async with InMemoryRunner(agent=root_agent) as runner:
-        user_id = "user_123"
-        session_id = "session_456"
-        
+        user_id = f"user_123_{random.random()}_{random.random()}"
+        session_id = f"session_456_{random.random()}_{random.random()}"
+
+
+        initial_state = {
+            "technical_report": None,
+            "fundamental_report": None,
+            "ticker": "AAPL"
+        }
+
+
         # Create a session
         await runner.session_service.create_session(
             app_name=runner.app_name,
             user_id=user_id,
-            session_id=session_id
+            session_id=session_id,
+            state=initial_state
         )
         
         print(f"Starting session {session_id} for user {user_id}...")
         
         # Call the agent
-        new_message = types.Content(parts=[types.Part(text="NVDA")])
+        new_message = types.Content(parts=[types.Part(text="Start the analysis")])
         
         async for event in runner.run_async(
             user_id=user_id,
@@ -36,7 +45,7 @@ async def main():
             if event.content and event.content.parts:
                 for part in event.content.parts:
                     if part.text:
-                        print(f"Agent: {part.text}")
+                        print(f"---- ---- Agent: {event.author} :: {part.text}")
 
         # Cleanup the session explicitly before exiting the context
         print(f"Cleaning up session {session_id}...")
